f79eeb49d6c213ce2cb9ca8abe2f6ed7ceb12596
Emil Ölund <emil.olund@jayway.com>
2022-11-22 02:38:42 +0100

    test

--- src/Button/Button.test.tsx
+++ src/Button/Button.test.tsx
@@ -6,14 +6,16 @@ import Button from './Button';
 describe('Running Test for Button', () => {
   test('Check Button Variants', async () => {
     const { findByRole, rerender } = render(
-      <Button variant={'primary'} size={'medium'} text="Button" />
+      <Button variant={'primary'} outline size={'medium'} text="Button" />
     );
 
     const buttonElement = await findByRole('button');
 
     expect(buttonElement.classList.contains('text-primary-default')).toBe(true);
 
-    rerender(<Button variant={'secondary'} size={'medium'} text="Button" />);
+    rerender(
+      <Button variant={'secondary'} outline size={'medium'} text="Button" />
+    );
 
     expect(buttonElement.classList.contains('text-secondary-default')).toBe(
       true
--- src/Input/Input.test.tsx
+++ src/Input/Input.test.tsx
@@ -5,10 +5,10 @@ import Input from './Input';
 
 describe('Running Test for Input', () => {
   test('Check Input Variants', async () => {
-    const { findByRole } = render(<Input value="Hej" warning />);
+    const { getByDisplayValue } = render(<Input value="Hej" warning />);
 
-    const buttonElement = await findByRole('textbox');
+    const inputElement = getByDisplayValue('Hej');
 
-    expect(buttonElement.classList.contains('text-primary-default')).toBe(true);
+    expect(inputElement.classList.contains('ring-accent-default')).toBe(true);
   });
 });
